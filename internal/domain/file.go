8c67d54c3c7b5660eeacf32336456909f2665d6a
Lars Artmann <git@lars.software>
2026-03-28 02:32:33 +0100

    refactor: Use package-local type reference in FileNode
    Simplify type references in FileNode struct and methods by using the
    package-local HTML type directly instead of the fully-qualified domain.HTML.
    
    Context:
    - The FileNode type is defined in the domain package
    - The HTML type alias was recently added to domain package (commit 760adad)
    - Since both types are in the same package, fully-qualified references
      are unnecessary and reduce code clarity
    
    Changes:
    - internal/domain/file.go:3 references to domain.HTML simplified to HTML
      - FileNode.html field declaration
      - FileNode.HTML() return type
      - FileNode.SetHTML() parameter type
    
    This is a cosmetic refactor following the introduction of the HTML type
    alias that eliminated the html/template dependency in the domain package.

--- internal/domain/file.go
+++ internal/domain/file.go
@@ -40,7 +40,7 @@ type FileNode struct {
 	path     URLPath
 	title    string
 	content  []byte
-	html     domain.HTML
+	html     HTML
 	toc      []TOCItem
 	metadata Frontmatter
 	modified time.Time
@@ -91,14 +91,14 @@ func (f *FileNode) Size() uint64 { return f.size }
 func (f *FileNode) Content() []byte { return f.content }
 
 // HTML returns the rendered HTML.
-func (f *FileNode) HTML() domain.HTML {
+func (f *FileNode) HTML() HTML {
 	return f.html
 }
 
 // SetHTML sets the rendered HTML.
 //
 // Deprecated: Mutates the FileNode. Will be replaced by immutable RenderedFile pattern.
-func (f *FileNode) SetHTML(html domain.HTML) {
+func (f *FileNode) SetHTML(html HTML) {
 	f.html = html
 }
 
